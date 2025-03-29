be93347cca0c22c1c63ec4c61896487253ecf295
Tuan Hoang <tqhoang@elrepo.org>
2025-03-29 17:59:21 -0400

    Add copyright and license to C++ code Rename README file
    Signed-off-by: Tuan Hoang <tqhoang@elrepo.org>

--- getkmoddevs/lsdevname.cpp
+++ getkmoddevs/lsdevname.cpp
@@ -1,15 +1,28 @@
 /*
- * Author:
- * Tuan Hoang <tqhoang@elrepo.org>
+ *  lsdevname - Looks up the given PCI/USB VendorID and DeviceID
+ *              and prints the corresponding names.
  *
- * Description:
- * This app looks up the given PCI/USB VendorID and DeviceID and 
- * prints the corresponding names.
+ *  Note:
+ *  For unknown vendors, it prints "UNKNOWN VENDOR <vendorID>"
+ *  For unknown devices, it prints "UNKNOWN DEVICE <deviceID>"
  *
- * Note:
- * For unknown vendors, it prints "UNKNOWN VENDOR <vendorID>"
- * For unknown devices, it prints "UNKNOWN DEVICE <deviceID>"
+ *  Copyright (C) 2024-2025 Tuan Hoang <tqhoang@elrepo.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  */
+
 #include <algorithm>
 #include <cctype>
 #include <fstream>
